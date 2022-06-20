3d3b7676269534a121b76701d91acbc8780ef3c4
Vladyslav <51026693+vladyslav-dev@users.noreply.github.com>
2022-06-20 16:02:38 +0300

    Added API_URL for dev mode

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { IAuthResponse } from '../interfaces/auth';
 
+const API_URL = 'http://localhost:8080';
+
 const $api = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL: `/api`,
+  baseURL: `${API_URL}/api`,
 });
 
 $api.interceptors.request.use((config: any) => {
@@ -21,7 +23,7 @@ $api.interceptors.response.use((congif) => {
   if (error?.response?.status === 401 && error?.config && !error?.config?._isRetry) {
       originalRequest._isRetry = true;
       try {
-        const response = await axios.get<IAuthResponse>(`/api/refresh`, { withCredentials: true });
+        const response = await axios.get<IAuthResponse>(`${API_URL}/api/refresh`, { withCredentials: true });
         localStorage.setItem('password-manager:accessToken', response?.data?.accessToken);
         return $api.request(originalRequest);
       } catch (err) {
